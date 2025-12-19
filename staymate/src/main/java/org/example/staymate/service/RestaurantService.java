181c463a4131a50eaf279162270d9669b27f158e
yash57-ty <thummaryash969@gmail.com>
2025-12-20 00:15:26 +0530

    many changes done

--- staymate/src/main/java/org/example/staymate/service/RestaurantService.java
+++ staymate/src/main/java/org/example/staymate/service/RestaurantService.java
@@ -22,7 +22,7 @@ public class RestaurantService {
             System.out.println("Phone: " + res.getPhone());
             System.out.println("Message: " + text);
         }
-        // many changes done
+        // many many changes done
     }
 }
 
